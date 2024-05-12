0c549b4861758e8c39419dc7c874fc5e2f5eae95
andras <kereszturiandras90@gmail.com>
2024-05-12 23:53:01 +0200

    add DB

--- Szakdoga8/Translations/TranslationsContext.cs
+++ Szakdoga8/Translations/TranslationsContext.cs
@@ -19,7 +19,7 @@ public partial class TranslationsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Translations;Trusted_Connection=True;");
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Translations\\Translations.mdf;Integrated Security=true;MultipleActiveResultSets=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
